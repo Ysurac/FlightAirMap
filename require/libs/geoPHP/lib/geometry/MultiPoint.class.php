b1e78c04fd97eb60eaf67976850436342e05eb3f
Ycarus <ycarus@zugaina.org>
2014-10-24 21:59:57 +0200

    Add GeoPHP needed for airspace

---
+++ require/libs/geoPHP/lib/geometry/MultiPoint.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * MultiPoint: A collection Points  
+ */
+class MultiPoint extends Collection
+{
+  protected $geom_type = 'MultiPoint';
+  
+  public function numPoints() {
+    return $this->numGeometries();
+  }
+  
+  public function isSimple() {
+    return TRUE;
+  }
+  
+  // Not valid for this geometry type
+  // --------------------------------
+  public function explode() { return NULL; }
+}
+
