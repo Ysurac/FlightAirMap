fe6bbd40d4870fff273a3006056b79c4eaacf947
Ycarus <ycarus@zugaina.org>
2015-04-18 07:27:30 +0200

    Fix schema version

--- db/config.sql
+++ db/config.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS `config` (
   `value` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `config` (`id`, `name`, `value`) VALUES (1, 'schema_version', '2');
+INSERT INTO `config` (`id`, `name`, `value`) VALUES (1, 'schema_version', '5');
 ALTER TABLE `config` ADD PRIMARY KEY (`id`);
 ALTER TABLE `config` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
