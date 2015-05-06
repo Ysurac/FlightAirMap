62af14837966496f2cb077d775a31bcc34e40122
Ycarus <ycarus@zugaina.org>
2015-05-06 17:48:10 +0200

    New version of Marker.Rotate, keep old position of the aircraft

--- js/Marker.Rotate.js
+++ js/Marker.Rotate.js
@@ -1,32 +1,51 @@
 /*
- * Based on comments by @runanet and @coomsie
+ * Based on comments by @runanet and @coomsie 
  * https://github.com/CloudMade/Leaflet/issues/386
  *
  * Wrapping function is needed to preserve L.Marker.update function
  */
 (function () {
-var _old_update = L.Marker.prototype.update;
-L.Marker.include({
-        update: function() {
-                this._icon.style[L.DomUtil.TRANSFORM] = "";
-                _old_update.apply(this, []);
+	var _old__setPos = L.Marker.prototype._setPos;
+	L.Marker.include({
+		_updateImg: function(i, a, s) {
+			a = L.point(s).divideBy(2)._subtract(L.point(a));
+			var transform = '';
+			transform += ' translate(' + -a.x + 'px, ' + -a.y + 'px)';
+			transform += ' rotate(' + this.options.iconAngle + 'deg)';
+			transform += ' translate(' + a.x + 'px, ' + a.y + 'px)';
+			i.style[L.DomUtil.TRANSFORM] += transform;
+		},
 
-                if (this.options.iconAngle) {
-                        var a = this.options.icon.options.iconAnchor;
-                        var s = this.options.icon.options.iconSize;
-                        a = L.point(s).divideBy(2)._subtract(L.point(a));
-                        var transform = '';
-                        transform += ' translate(' + -a.x + 'px, ' + -a.y + 'px)';
-                        transform += ' rotate(' + this.options.iconAngle + 'deg)';
-                        transform += ' translate(' + a.x + 'px, ' + a.y + 'px)';
-                        this._icon.style[L.DomUtil.TRANSFORM] += transform;
-                }
-        },
+		setIconAngle: function (iconAngle) {
+			this.options.iconAngle = iconAngle;
+			if (this._map)
+				this.update();
+		},
 
-        setIconAngle: function (iconAngle) {
-                this.options.iconAngle = iconAngle;
+		_setPos: function (pos) {
+			if (this._icon)
+				this._icon.style[L.DomUtil.TRANSFORM] = '';
+			if (this._shadow)
+				this._shadow.style[L.DomUtil.TRANSFORM] = '';
 
-                if (this._map) this.update();
-        }
-});
+			_old__setPos.apply(this,[pos]);
+
+			if (this.options.iconAngle) {
+				var a = this.options.icon.options.iconAnchor;
+				var s = this.options.icon.options.iconSize;
+				var i;
+				if (this._icon) {
+					i = this._icon;
+					this._updateImg(i, a, s);
+				}
+				if (this._shadow) {
+					if (this.options.icon.options.shadowAnchor)
+						a = this.options.icon.options.shadowAnchor;
+					s = this.options.icon.options.shadowSize;
+					i = this._shadow;
+					this._updateImg(i, a, s);
+				}
+			}
+		}
+	});
 }());
