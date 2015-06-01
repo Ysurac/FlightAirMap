ee9da2cf1be156a9ac8a919c3d82cc8dd5145428
Ycarus <ycarus@zugaina.org>
2015-06-01 20:28:07 +0200

    Allow more diff between latitude

--- require/class.SBS.php
+++ require/class.SBS.php
@@ -121,7 +121,7 @@ class SBS {
 		}
 	        
 		if (isset($line['latitude']) && $line['latitude'] != '' && $line['latitude'] != 0 && $line['latitude'] < 91) {
-		    if (!isset(self::$all_flights[$id]['latitude']) || self::$all_flights[$id]['latitude'] == '' || abs(self::$all_flights[$id]['latitude']-$line['latitude']) < 2 || $line['format_source'] != 'sbs') {
+		    if (!isset(self::$all_flights[$id]['latitude']) || self::$all_flights[$id]['latitude'] == '' || abs(self::$all_flights[$id]['latitude']-$line['latitude']) < 3 || $line['format_source'] != 'sbs') {
 			if (!isset(self::$all_flights[$id]['archive_latitude'])) self::$all_flights[$id]['archive_latitude'] = $line['latitude'];
 			if (!isset(self::$all_flights[$id]['livedb_latitude']) || abs(self::$all_flights[$id]['livedb_latitude']-$line['latitude']) > 0.02) {
 			    self::$all_flights[$id]['livedb_latitude'] = $line['latitude'];
