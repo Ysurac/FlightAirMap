78f5481ca65a70f05a0b45f37595bd9c447e4d0d
Ycarus <ycarus@zugaina.org>
2015-06-11 09:12:52 +0200

    Fix location geojson

--- location-geojson.php
+++ location-geojson.php
@@ -45,8 +45,8 @@ if (!empty($spotter_array))
 		    $output .= '}';
 		$output .= '},';
 	}
+	$output  = substr($output, 0, -1);
 }
-$output  = substr($output, 0, -1);
 $output .= ']}';
 
 print $output;
