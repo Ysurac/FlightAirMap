404b9bee34f844d3ce3b86ea0ec9b56990d628fb
Ycarus <ycarus@zugaina.org>
2015-10-23 09:43:45 +0200

    Fix undefined var

--- require/class.Spotter.php
+++ require/class.Spotter.php
@@ -4094,7 +4094,7 @@ class Spotter{
 			if($row['registration'] != "")
 			{
 				$image_array = $Image->getSpotterImage($row['registration']);
-				$temp_array['image_thumbnail'] = $image_array[0]['image_thumbnail'];
+				if (isset($image_array[0]['image_thumbnail'])) $temp_array['image_thumbnail'] = $image_array[0]['image_thumbnail'];
 			}
 			$temp_array['registration_count'] = $row['registration_count'];
           
