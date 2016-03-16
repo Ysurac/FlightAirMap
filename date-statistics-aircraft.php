77deb72854b1fd70737a0ceb061c08540dc0cf1d
Ycarus <ycarus@zugaina.org>
2016-03-16 16:32:19 +0100

    Small fix with sort

--- date-statistics-aircraft.php
+++ date-statistics-aircraft.php
@@ -2,81 +2,70 @@
 require_once('require/class.Connection.php');
 require_once('require/class.Spotter.php');
 $Spotter = new Spotter();
-$spotter_array = $Spotter->getSpotterDataByDate($_GET['date'],"0,1", $_GET['sort']);
+if (isset($_GET['sort'])) $sort = $_GET['sort'];
+else $sort = '';
+if (isset($_GET['date'])) $spotter_array = $Spotter->getSpotterDataByDate($_GET['date'],"0,1", $sort);
+else $spotter_array = array();
 
 if (!empty($spotter_array))
 {
-  
-  
-  $title = 'Most Common Aircraft on '.date("l F j, Y", strtotime($spotter_array[0]['date_iso_8601']));
+	$title = 'Most Common Aircraft on '.date("l F j, Y", strtotime($spotter_array[0]['date_iso_8601']));
 	require_once('header.php');
-	
-  print '<div class="select-item">';
-  		print '<form action="'.$globalURL.'/date" method="post">';
-  			print '<label for="date">Select a Date</label>';
-    		print '<input type="text" id="date" name="date" value="'.$_GET['date'].'" size="8" readonly="readonly" class="custom" />';
-    		print '<button type="submit"><i class="fa fa-angle-double-right"></i></button>';
-  		print '</form>';
-  	print '</div>';
-  
-  print '<div class="info column">';
-  	print '<h1>Flights from '.date("l F j, Y", strtotime($spotter_array[0]['date_iso_8601'])).'</h1>';
-  print '</div>';
+	print '<div class="select-item">';
+	print '<form action="'.$globalURL.'/date" method="post">';
+	print '<label for="date">Select a Date</label>';
+	print '<input type="text" id="date" name="date" value="'.$_GET['date'].'" size="8" readonly="readonly" class="custom" />';
+	print '<button type="submit"><i class="fa fa-angle-double-right"></i></button>';
+	print '</form>';
+	print '</div>';
 
-  include('date-sub-menu.php');
-  
-  print '<div class="column">';
-  	print '<h2>Most Common Aircraft</h2>';
-  	print '<p>The statistic below shows the most common aircrafts of flights on <strong>'.date("l F j, Y", strtotime($spotter_array[0]['date_iso_8601'])).'</strong>.</p>';
+	print '<div class="info column">';
+	print '<h1>Flights from '.date("l F j, Y", strtotime($spotter_array[0]['date_iso_8601'])).'</h1>';
+	print '</div>';
 
-	  $aircraft_array = $Spotter->countAllAircraftTypesByDate($_GET['date']);
-	  
-	  if (!empty($aircraft_array))
-	  {
-	    print '<div class="table-responsive">';
-		    print '<table class="common-type table-striped">';
-		      print '<thead>';
-		        print '<th></th>';
-		        print '<th>Aircraft Type</th>';
-		        print '<th># of Times</th>';
-		        print '<th></th>';
-		      print '</thead>';
-		      print '<tbody>';
-		      $i = 1;
-		        foreach($aircraft_array as $aircraft_item)
-		        {
-		          print '<tr>';
-		          	print '<td><strong>'.$i.'</strong></td>';
-		            print '<td>';
-		              print '<a href="'.$globalURL.'/aircraft/'.$aircraft_item['aircraft_icao'].'">'.$aircraft_item['aircraft_name'].' ('.$aircraft_item['aircraft_icao'].')</a>';
-		            print '</td>';
-		            print '<td>';
-		              print $aircraft_item['aircraft_icao_count'];
-		            print '</td>';
-		            print '<td><a href="'.$globalURL.'/search?aircraft='.$aircraft_item['aircraft_icao'].'&start_date='.$_GET['date'].'+00:00&end_date='.$_GET['date'].'+23:59">Search flights</a></td>';
-		          print '</tr>';
-		          $i++;
-		        }
-		      print '<tbody>';
-		    print '</table>';
-	    print '</div>';
-	  }
-  print '</div>';
-  
-  
+	include('date-sub-menu.php');
+	print '<div class="column">';
+	print '<h2>Most Common Aircraft</h2>';
+	print '<p>The statistic below shows the most common aircrafts of flights on <strong>'.date("l F j, Y", strtotime($spotter_array[0]['date_iso_8601'])).'</strong>.</p>';
+
+	$aircraft_array = $Spotter->countAllAircraftTypesByDate($_GET['date']);
+	if (!empty($aircraft_array))
+	{
+		print '<div class="table-responsive">';
+		print '<table class="common-type table-striped">';
+		print '<thead>';
+		print '<th></th>';
+		print '<th>Aircraft Type</th>';
+		print '<th># of Times</th>';
+		print '<th></th>';
+		print '</thead>';
+		print '<tbody>';
+		$i = 1;
+		foreach($aircraft_array as $aircraft_item)
+		{
+			print '<tr>';
+			print '<td><strong>'.$i.'</strong></td>';
+			print '<td>';
+			print '<a href="'.$globalURL.'/aircraft/'.$aircraft_item['aircraft_icao'].'">'.$aircraft_item['aircraft_name'].' ('.$aircraft_item['aircraft_icao'].')</a>';
+			print '</td>';
+			print '<td>';
+			print $aircraft_item['aircraft_icao_count'];
+			print '</td>';
+			print '<td><a href="'.$globalURL.'/search?aircraft='.$aircraft_item['aircraft_icao'].'&start_date='.$_GET['date'].'+00:00&end_date='.$_GET['date'].'+23:59">Search flights</a></td>';
+			print '</tr>';
+			$i++;
+		}
+		print '<tbody>';
+		print '</table>';
+		print '</div>';
+	}
+	print '</div>';
 } else {
-
 	$title = "Unknown Date";
 	require_once('header.php');
-	
 	print '<h1>Error</h1>';
-
-  print '<p>Sorry, this date does not exist in this database. :(</p>'; 
+	print '<p>Sorry, this date does not exist in this database. :(</p>'; 
 }
 
-
-?>
-
-<?php
 require_once('footer.php');
 ?>
