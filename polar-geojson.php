65be4bae4203599c34d74945eda172b1b6511db5
Ycarus <ycarus@zugaina.org>
2016-08-04 14:54:57 +0200

    Fix

--- polar-geojson.php
+++ polar-geojson.php
@@ -9,44 +9,41 @@ $Location = new Source();
 $Common = new Common();
 
 if (isset($_GET['download'])) {
-    if ($_GET['download'] == "true")
-    {
-	header('Content-disposition: attachment; filename="flightairmap.json"');
-    }
+	if ($_GET['download'] == "true")
+	{
+		header('Content-disposition: attachment; filename="flightairmap-polar.json"');
+	}
 }
 header('Content-Type: text/javascript');
 
 
 $polar = $Stats->getStatsSource(date('Y-m-d'),'polar');
-$output = '';
+$output = '{"type": "FeatureCollection","features": [';
 if (!empty($polar)) {
-    $output = '{"type": "FeatureCollection","features": [';
-
-    foreach($polar as $eachpolar) {
-	$data = json_decode($eachpolar['source_data']);
-	$name = $eachpolar['source_name'];
-	$coord = $Location->getLocationInfobySourceName($name);
-        $output .= '{"type": "Feature","properties": {"name": "'.$name.'","style": {"color": "#D3FFCF", "opacity": 0.5}},"geometry": {"type": "Polygon","coordinates": [[';
-	if (isset($coord[0]['latitude'])) {
-		$initial_latitude = $coord[0]['latitude'];
-		$initial_longitude = $coord[0]['longitude'];
-	} else {
-		$initial_latitude = $globalCenterLatitude;
-		$initial_longitude = $globalCenterLongitude;
+	foreach($polar as $eachpolar) {
+		$data = json_decode($eachpolar['source_data']);
+		$name = $eachpolar['source_name'];
+		$coord = $Location->getLocationInfobySourceName($name);
+		$output .= '{"type": "Feature","properties": {"name": "'.$name.'","style": {"color": "#D3FFCF", "opacity": 0.5}},"geometry": {"type": "Polygon","coordinates": [[';
+		if (isset($coord[0]['latitude'])) {
+			$initial_latitude = $coord[0]['latitude'];
+			$initial_longitude = $coord[0]['longitude'];
+		} else {
+			$initial_latitude = $globalCenterLatitude;
+			$initial_longitude = $globalCenterLongitude;
+		}
+		$first = '';
+		foreach($data as $value => $key) {
+			$final_coord = $Common->getCoordfromDistanceBearing($initial_latitude,$initial_longitude,$value*22.5,$key);
+			if ($first == '') $first = '['.round($final_coord['longitude'],5).','.round($final_coord['latitude'],5).']';
+			$output .= '['.$final_coord['longitude'].','.$final_coord['latitude'].'],';
+		}
+		$output .= $first;
+		$output .= ']]}},';
 	}
-	$first = '';
-	foreach($data as $value => $key) {
-		$final_coord = $Common->getCoordfromDistanceBearing($initial_latitude,$initial_longitude,$value*22.5,$key);
-		if ($first == '') $first = '['.round($final_coord['longitude'],5).','.round($final_coord['latitude'],5).']';
-		$output .= '['.$final_coord['longitude'].','.$final_coord['latitude'].'],';
-	}
-	//$output  = substr($output, 0, -1);
-	$output .= $first;
-	$output .= ']]}},';
-    }
-    $output  = substr($output, 0, -1);
-    $output .= ']}';
+	$output  = substr($output, 0, -1);
 }
+$output .= ']}';
 print $output;
 
 ?>
