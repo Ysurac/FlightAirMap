8ae58304a255c0ff167848a6642a5e02ced14dfc
Ycarus <ycarus@zugaina.org>
2016-10-24 18:57:53 +0200

    Fix VAM support

--- scripts/daemon-spotter.php
+++ scripts/daemon-spotter.php
@@ -751,11 +751,11 @@ while ($i > 0) {
 		foreach ($all_data as $line) {
 	    	    $data = array();
 	    	    //$data['id'] = $line['id']; // id not usable
-	    	    $data['id'] = $line['flight_id'];
+	    	    $data['id'] = trim($line['flight_id']);
 	    	    $data['hex'] = substr(str_pad(bin2hex($line['callsign']),6,'000000',STR_PAD_LEFT),-6); // hex
 	    	    $data['pilot_name'] = $line['pilot_name'];
 	    	    $data['pilot_id'] = $line['pilot_id'];
-	    	    $data['ident'] = $line['callsign']; // ident
+	    	    $data['ident'] = trim($line['callsign']); // ident
 	    	    $data['altitude'] = $line['altitude']; // altitude
 	    	    $data['speed'] = $line['gs']; // speed
 	    	    $data['heading'] = $line['heading']; // heading
