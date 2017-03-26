5e832498483a2dbe129ad29d006889894a0fd514
Ycarus <ycarus@zugaina.org>
2017-03-26 19:39:25 +0200

    Fix #280 again

--- incident.php
+++ incident.php
@@ -1,4 +1,5 @@
 <?php
+require_once(dirname(__FILE__).'/require/settings.php');
 $date = filter_input(INPUT_POST,'date',FILTER_SANITIZE_STRING);
 if ($date == '') $date = date('Y-m-d');
 header('Location: '.$globalURL.'/incident/'.$date);
