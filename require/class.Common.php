29f11a46347b341f9d1bcf96e0d09c2c538a1c03
Ycarus <ycarus@zugaina.org>
2017-07-11 16:52:24 +0200

    Fix is writable function

--- require/class.Common.php
+++ require/class.Common.php
@@ -665,9 +665,9 @@ class Common {
 		//see http://bugs.php.net/bug.php?id=27609
 		//see http://bugs.php.net/bug.php?id=30931
 		if ($path{strlen($path)-1}=='/') // recursively return a temporary file path
-			return is__writable($path.uniqid(mt_rand()).'.tmp');
+			return $this->is__writable($path.uniqid(mt_rand()).'.tmp');
 		else if (is_dir($path))
-			return is__writable($path.'/'.uniqid(mt_rand()).'.tmp');
+			return $this->is__writable($path.'/'.uniqid(mt_rand()).'.tmp');
 		// check tmp file for read/write capabilities
 		$rm = file_exists($path);
 		$f = @fopen($path, 'a');
