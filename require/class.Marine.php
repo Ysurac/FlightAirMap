83686fd7fd8e582b444df3f1f31da7a04e75ae4c
Ycarus <ycarus@zugaina.org>
2017-08-12 08:01:28 +0200

    add ship name to marine identity table if diff

--- require/class.Marine.php
+++ require/class.Marine.php
@@ -476,10 +476,15 @@ class Marine{
 			$ident = filter_var($ident,FILTER_SANITIZE_STRING);
 			$callsign = filter_var($callsign,FILTER_SANITIZE_STRING);
 			$type = filter_var($type,FILTER_SANITIZE_STRING);
-			if (empty($this->getIdentity($mmsi))) {
+			$identinfo = $this->getIdentity($mmsi);
+			if ($identinfo == '') {
 				$query  = "INSERT INTO marine_identity (mmsi,imo,call_sign,ship_name,type) VALUES (:mmsi,:imo,:call_sign,:ship_name,:type)";
 				$sth = $this->db->prepare($query);
 				$sth->execute(array(':mmsi' => $mmsi,':imo' => $imo,':call_sign' => $callsign,':ship_name' => $ident,':type' => $type));
+			} elseif ($identinfo != $ident) {
+				$query  = "UPDATE marine_identity SET ship_name = :ship_name,type = :type WHERE mmsi = :mmsi";
+				$sth = $this->db->prepare($query);
+				$sth->execute(array(':mmsi' => $mmsi,':ship_name' => $ident,':type' => $type));
 			}
 		}
 	}
