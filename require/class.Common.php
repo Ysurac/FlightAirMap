31027a2768d440fbeacd0b8b4159cca46be553d6
Ycarus <ycarus@zugaina.org>
2017-10-02 13:56:39 +0200

    Optimize

--- require/class.Common.php
+++ require/class.Common.php
@@ -776,7 +776,8 @@ class Common {
 		$dfDiffSpace = 360 - $dfDateLineOffset;
 		
 		// https://github.com/OSGeo/gdal/blob/7bfb9c452a59aac958bff0c8386b891edf8154ca/gdal/ogr/ogrgeometryfactory.cpp#L2342
-		for ($j = 1; $j < count($first_pass); ++$j) {
+		$first_pass_ln = count($first_pass);
+		for ($j = 1; $j < $first_pass_ln; ++$j) {
 			$dfPrevX = $first_pass[$j-1][0];
 			$dfX = $first_pass[$j][0];
 			$dfDiffLong = abs($dfX - $dfPrevX);
@@ -789,10 +790,11 @@ class Common {
 			}
 		}
 		$poMulti = array();
+		$first_pass_ln = count($first_pass);
 		if ($bHasBigDiff && $dfMaxSmallDiffLong < $dfDateLineOffset) {
 			$poNewLS = array();
 			//$poMulti[] = $poNewLS;
-			for ($k = 0; $k < count($first_pass); ++$k) {
+			for ($k = 0; $k < $first_pass_ln; ++$k) {
 				$dfX0 = floatval($first_pass[$k][0]);
 				if ($k > 0 &&  abs($dfX0 - $first_pass[$k-1][0]) > $dfDiffSpace) {
 					$dfX1 = floatval($first_pass[$k-1][0]);
@@ -809,7 +811,7 @@ class Common {
 						$poNewLS[] = array($first_pass[$k][0], $first_pass[$k][1]);
 						continue;
 					} else if ($dfX1 > $dfLeftBorderX && $dfX1 < 180 && $dfX2 == -180 &&
-					    $k+1 < count($first_pass) &&
+					    $k+1 < $first_pass_ln &&
 					    $first_pass[$k-1][0] > $dfLeftBorderX && $first_pass[$k-1][0] < 180)
 					{
 						$poNewLS[] = array(180, $first_pass[$k][1]);
@@ -855,7 +857,7 @@ class Common {
 			// add normally
 			$poNewLS0 = array();
 			//$poMulti[] = $poNewLS0;
-			for ($l = 0; $l < count($first_pass); ++$l) {
+			for ($l = 0; $l < $first_pass_ln; ++$l) {
 				$poNewLS0[] = array($first_pass[$l][0],$first_pass[$l][1]);
 			}
 			$poMulti[] = $poNewLS0;
