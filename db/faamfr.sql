d62be9282e954036d787c2ea3fe23f6f5dca5859
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-19 07:31:39 +0800

    Delete db directory

--- db/faamfr.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `faamfr` ( 
-    `faamfr_id` INT NOT NULL AUTO_INCREMENT,
-    `icao` VARCHAR(10) NOT NULL,
-    `mfr` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`faamfr_id`),
-    UNIQUE (`mfr`)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
--- db/pgsql/faamfr.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE faamfr (
-    faamfr_id serial,
-    icao VARCHAR(10) NOT NULL,
-    mfr VARCHAR(255) NOT NULL,
-    PRIMARY KEY (faamfr_id),
-    UNIQUE (mfr)
-);
